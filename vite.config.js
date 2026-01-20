493e5380abb857150af6770c1455719508b8a851
devJeremie <chabanaisjeremie@gmail.com>
2026-01-20 10:03:59 +0100

    Start-Project

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+const __dirname = import.meta.dirname; //a voir selon les versions de nodes (facultatif)
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
