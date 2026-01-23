42c50bcf33d67fb0a4228b117f6efc2afae3aa53
devJeremie <chabanaisjeremie@gmail.com>
2026-01-23 08:13:02 +0100

    Start-Projects-Add-Pictures

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,3 +1,11 @@
+const projects = [
+    {
+        title: "Portfolio Website",
+        description: "A personal portfolio website to showcase my projects and skills.",
+        link: ""
+    }
+]
+
 export const Projects = () => {
     return <section></section>;
 }
