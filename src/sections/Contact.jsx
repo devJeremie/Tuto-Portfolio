864495d1cab86a1eb8275c0e0fd143681f2f3a35
devJeremie <chabanaisjeremie@gmail.com>
2026-03-11 10:02:01 +0100

    Maj-Button-...props

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,4 +1,5 @@
-import {Mail, Phone, Location} from "lucide-react";
+import {Mail, Phone, Location, Send} from "lucide-react";
+import {Button} from "@/components/Button"
 
 const contactInfo = [
     {
@@ -50,56 +51,60 @@ export const Contact = () => {
           {/*Formulaire*/}
           <div className="grid lg:grid-cols-2 gap-12 max-w-5xl mx-auto">
             <div className="glass p-8 rounded-3xl border border-primary/30 animate-fade-in animation-delay-300">
-              <form className="space-y-6">
-                <div>
-                  <label
-                    htmlFor="name"
-                    className="block text-sm font-medium mb-2 "
-                  >
-                    Nom
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    required
-                    placeholder="Votre nom.."
-                    className="w-full px-4 py-3 bg-surface rounded-xl border border-border transition-all 
-                                    focus:border-primary focus:ring-1 focus:ring-primary outline-none"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium mb-2 "
-                  >
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    required
-                    placeholder="votre@email.com"
-                    className="w-full px-4 py-3 bg-surface rounded-xl border border-border transition-all 
-                                    focus:border-primary focus:ring-1 focus:ring-primary outline-none"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="message"
-                    className="block text-sm font-medium mb-2 "
-                  >
-                    Message
-                  </label>
-                  <textarea
-                    rows={5}
-                    required
-                    placeholder="Votre message.."
-                    className="w-full px-4 py-3 bg-surface rounded-xl border border-border transition-all 
-                                    focus:border-primary focus:ring-1 focus:ring-primary outline-none
-                                    resize-none"
-                  />
-                </div>
-              </form>
+                <form className="space-y-6">
+                    <div>
+                        <label
+                            htmlFor="name"
+                            className="block text-sm font-medium mb-2 "
+                        >
+                            Nom
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            required
+                            placeholder="Votre nom.."
+                            className="w-full px-4 py-3 bg-surface rounded-xl border border-border transition-all 
+                                            focus:border-primary focus:ring-1 focus:ring-primary outline-none"
+                        />
+                    </div>
+                    <div>
+                        <label
+                            htmlFor="email"
+                            className="block text-sm font-medium mb-2 "
+                        >
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            required
+                            placeholder="votre@email.com"
+                            className="w-full px-4 py-3 bg-surface rounded-xl border border-border transition-all 
+                                            focus:border-primary focus:ring-1 focus:ring-primary outline-none"
+                        />
+                    </div>
+                    <div>
+                        <label
+                            htmlFor="message"
+                            className="block text-sm font-medium mb-2 "
+                        >
+                            Message
+                        </label>
+                        <textarea
+                            rows={5}
+                            required
+                            placeholder="Votre message.."
+                            className="w-full px-4 py-3 bg-surface rounded-xl border border-border transition-all 
+                                            focus:border-primary focus:ring-1 focus:ring-primary outline-none
+                                            resize-none"
+                        />
+                    </div>
+                    <Button className="w-full" type="submit" size="lg">
+                        Envoyer le message
+                        <Send />
+                    </Button>
+                </form>
             </div>
           </div>
         </div>
