2da87089fddd6f24a709637a9cb42ba0fe9506f5
devJeremie <chabanaisjeremie@gmail.com>
2026-03-11 08:21:43 +0100

    Maj-Start-Contact

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,3 +1,34 @@
+const contactInfo = [
+    {
+        icon: Mail,
+        label: "Email",
+        value: "chabanaisjeremie@gmail.com",
+        href: "mailto:chabanaisjeremie@gmail.com"
+    },
+      {
+        icon: Phone,
+        label: "Tèl",
+        value: "+33 6 34 26 31 10",
+        href: "tel:+33634263110"
+    },
+      {
+        icon: Location,
+        label: "Location",
+        value: "Bordeaux, France",
+        href: "https://www.google.com/maps/place/Bordeaux"
+    }
+];
+
+export const Contact = () => {
+        href: 
+    },
+      {
+        icon: 
+        label: 
+        value: 
+        href: 
+    },
+
 export const Contact = () => {
     return <section></section>;
 }
